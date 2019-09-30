88d1f04cec5d065b1404a2d8d6e306912bc728c7
MatiasFernandez <mfernandez@10pines.com>
2019-09-30 00:14:38 -0300

    fix Rack::Cache configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,8 +18,14 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
-  config.action_dispatch.rack_cache = true
+  client = Dalli::Client.new('127.0.0.1:11211')
 
+  config.action_dispatch.rack_cache = {
+    :metastore    => client,
+    :entitystore  => client
+  }
+
+  config.static_cache_control = "public, max-age=2592000"
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = true
