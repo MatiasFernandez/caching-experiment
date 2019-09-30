05fb47eec862487224da5732449718ad370b8c7f
MatiasFernandez <mfernandez@10pines.com>
2019-09-30 09:15:11 -0300

    Add extra logging to debug rack middlewares execution

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+module LogMiddleware
+  def call(env)
+    Rails.logger.debug "Starting #{self.class.name} middleware"
+    result = super
+    Rails.logger.debug "#{self.class.name} middleware Finished"
+    result
+  end
+end
+
+Rails.configuration.middleware.map(&:klass).each do |middleware|
+  middleware.prepend LogMiddleware if middleware.respond_to?(:prepend)
+end
+
+Rails.application.routes.class.prepend LogMiddleware
+Rack::Cache::Context.prepend LogMiddleware
